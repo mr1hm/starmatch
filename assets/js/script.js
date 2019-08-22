b43bd745656228bc05644a8d945edc9a1c025391
Kevin Ihm <kevinihm@gmail.com>
2019-08-21 21:13:04 -0700

    added win condition

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,28 @@ $(document).ready(initializeApp);
 var firstCardClicked = null;
 var secondCardClicked = null;
 var matches = null;
+var max_matches = 2;
 
 function initializeApp() {
   $('.lfz-card').click(handleCardClick);
+  $('.modal-button').click(function () {
+    $('.modal').addClass('hidden');
+    $('.lfz-card').removeClass('hidden');
+    $('.container').removeClass('avoid-clicks');
+  })
 }
 
 function handleCardClick(event) {
   var target = $(event.currentTarget);
   $(target).addClass('hidden');
   cardsClicked();
+}
+
+function matchCheck() {
+  if (matches === max_matches) {
+    $('.modal').removeClass('hidden');
+    $('.container').addClass('avoid-clicks');
+  }
 }
 
 function cardsClicked() {
@@ -24,24 +37,25 @@ function cardsClicked() {
   if (firstCardClicked.siblings().css('background-image') === secondCardClicked.siblings().css('background-image')) {
     console.log('cards match');
     $('.lfz-card').addClass('avoid-clicks');
-    $('.container').css('background-color', 'white');
+    $('.container').css('background-color', 'rgba(0, 200, 0, 0.4');
     matches++;
     firstCardClicked = null;
     secondCardClicked = null;
+    matchCheck();
     setTimeout(function() {
       $('.lfz-card').removeClass('avoid-clicks');
-      $('.container').css('background-color', 'rgba(200, 0, 0, 0.4)');
+      $('.container').css('background-color', 'rgba(0, 0, 0, 0.4');
     }, 1500);
     } else if (firstCardClicked.siblings().css('background-image') !== secondCardClicked.siblings().css('background-image')) {
       $('.lfz-card').addClass('avoid-clicks');
-      $('.container').css('background-color', 'white');
+      $('.container').css('background-color', 'rgba(200, 0, 0, 0.4');
       setTimeout(function () {
       firstCardClicked.removeClass('hidden');
       firstCardClicked = null;
       secondCardClicked.removeClass('hidden');
       secondCardClicked = null;
       $('.lfz-card').removeClass('avoid-clicks');
-      $('.container').css('background-color', 'rgba(200, 0, 0, 0.4)');
+      $('.container').css('background-color', 'rgba(0, 0, 0, 0.4');
     }, 1500)
   }
   // IF TWO CARDS DON'T MATCH, YOU CAN FREELY CHECK OTHER CARDS, AND WHEN THEY MATCH, IT WILL KEEP THE UNMATCHED CARDS REVEALED!!
